a035d0a07f582fb0157ea2baceafac230ef1af21
“yumiazusa” <yumiazusa@hotmail.com>
2026-01-28 11:42:17 +0800

    1

---
+++ index.py
@@ -0,0 +1,9 @@
+'''
+Author: “yumiazusa” “yumiazusa@hotmail.com
+Date: 2026-01-28 11:34:05
+LastEditors: “yumiazusa” “yumiazusa@hotmail.com
+LastEditTime: 2026-01-28 11:34:17
+FilePath: /gittest/test1/index.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+print(1)
