fbb60d62f0a81ddd142bc9e7d83ef775e8e9a29e
“yumiazusa” <yumiazusa@hotmail.com>
2026-01-29 23:17:15 +0800

    main index4

--- index4.py
+++ index4.py
@@ -2,10 +2,11 @@
 Author: “yumiazusa” “yumiazusa@hotmail.com
 Date: 2026-01-28 13:04:12
 LastEditors: “yumiazusa” “yumiazusa@hotmail.com
-LastEditTime: 2026-01-29 22:27:38
+LastEditTime: 2026-01-29 23:17:03
 FilePath: /gittest/test1/index4.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 print(4)
 print(444)
 print(4444)
+print(44444444444444444)
